bc17c8c611c5cfa2b4372fa3bd4cb1e46b8565dc
Tomasz Morawski <tomekm1700@gmail.com>
2017-01-15 16:45:36 +0100

    Test case fix

--- src/main/java/examples/collectioninjection/SchoolService.java
+++ src/main/java/examples/collectioninjection/SchoolService.java
@@ -8,14 +8,19 @@ import java.util.Collection;
 public class SchoolService extends BindingContainer {
     private ArrayList<Teacher> teacherArrayList;
 
+    // Fails because configure() in BindingCointainer constructor is called before creating this array
+    /*
     public SchoolService() {
         this.teacherArrayList = new ArrayList<>();
         this.teacherArrayList.add(new Teacher("First"));
         this.teacherArrayList.add(new Teacher("Second"));
     }
-
+    */
     @Override
     public void configure() {
+        this.teacherArrayList = new ArrayList<>();
+        this.teacherArrayList.add(new Teacher("First"));
+        this.teacherArrayList.add(new Teacher("Second"));
         addBinding(Collection.class, teacherArrayList);
     }
 }
